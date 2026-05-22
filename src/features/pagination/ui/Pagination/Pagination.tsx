d3a08fed3093ac0d7db437f7132224c933e0a2b1
Niakrist <ak_88dron@inbox.ru>
2026-05-22 12:34:54 +0300

    feat: emplement feature/14

---
+++ src/features/pagination/ui/Pagination/Pagination.tsx
@@ -0,0 +1,39 @@
+"use client";
+import { Icon } from "@/shared/ui";
+import styles from "./Pagination.module.css";
+import { useState } from "react";
+import cn from "classnames";
+
+const PAGES = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+export const Pagination = () => {
+  const [currentIndex, setCurrentIndex] = useState<number | null>(null);
+
+  const handleClick = (index: number) => {
+    setCurrentIndex(index);
+  };
+
+  return (
+    <nav>
+      <ul className={styles.list}>
+        <li className={styles.item}>
+          <Icon name="iconPaginationLeft" />
+        </li>
+        {PAGES.map((page) => (
+          <li
+            onClick={() => handleClick(page)}
+            className={cn(styles.item, {
+              [styles.active]: currentIndex === page,
+            })}
+            key={page}
+          >
+            {page}
+          </li>
+        ))}
+        <li className={styles.item}>
+          <Icon name="iconPaginationRight" />
+        </li>
+      </ul>
+    </nav>
+  );
+};
