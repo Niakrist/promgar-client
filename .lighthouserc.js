3e3b54da6ee970d74db41a90e4a369cb5043fd2f
Niakrist <ak_88dron@inbox.ru>
2026-05-25 17:04:46 +0300

    Add Lighthouse CI workflow

---
+++ .lighthouserc.js
@@ -0,0 +1,30 @@
+module.exports = {
+  ci: {
+    collect: {
+      // Запускаем продакшн-сборку и поднимаем сервер
+      // startServerCommand: "npm run build && npm run start",
+      // startServerReadyPattern: "Ready in",
+      // Порт по умолчанию, если не задан иначе
+      url: [
+        "http://localhost:3000/", // главная
+        "http://localhost:3000/products/ball-bearings", // добавь свои маршруты
+        "http://localhost:3000/products/ball-bearings/6205", // добавь свои маршруты
+      ],
+      numberOfRuns: 3, // несколько прогонов для стабильности
+    },
+    assert: {
+      // Пороговые значения для демонстрации (потом настроим жёстче)
+      assertions: {
+        "categories:performance": ["error", { minScore: 0.8 }], // общая оценка не ниже 80 баллов.
+        "first-contentful-paint": ["error", { maxNumericValue: 2000 }], // FCP должен быть ≤ 2000 мс
+        "largest-contentful-paint": ["error", { maxNumericValue: 30000 }], // LCP ≤ 30000 мс
+        "cumulative-layout-shift": ["error", { maxNumericValue: 0.1 }], // CLS ≤ 0.1 (порог «хорошо»).
+        "total-blocking-time": ["error", { maxNumericValue: 300 }], // TBT ≤ 300 мс.
+      },
+    },
+    upload: {
+      // Пока будем выводить результаты локально, без загрузки в хранилище
+      target: "temporary-public-storage",
+    },
+  },
+};
