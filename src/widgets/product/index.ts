c2c1d1ee80a9f21089c1a1d46a1911697fca7db8
Niakrist <ak88dron@gmail.com>
2026-07-08 16:28:57 +0300

    feat: change rest api

--- src/widgets/product/index.ts
+++ src/widgets/product/index.ts
@@ -4,3 +4,4 @@ export { ProductPageNav } from "./productPageNav";
 export { ProductWrapper } from "./productWrapper";
 export { ProductSpecificationsWrapper } from "./productSpecificationsWrapper";
 export { ProductDescription } from "./productDescription";
+export { ProductAnalogWrapper } from './productAnalogWrapper'
